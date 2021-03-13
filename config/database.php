f4ff6093b819609069f36548ca80b142d4d226b4
arnold graaff <arnoldgr@gmail.com>
2021-03-13 14:53:44 +0200

    aaa

--- config/database.php
+++ config/database.php
@@ -2,10 +2,10 @@
 class Database{
 
 	// specify your own database credentials
-	private $host = "localhost";
-	private $db_name = "api_db";
-	private $username = "postgres";
-	private $password = "postgres";
+	private $host = "ec2-54-159-175-113.compute-1.amazonaws.com";
+	private $db_name = "d4c2023pgipanb";
+	private $username = "zeeptopmcapcdy";
+	private $password = "eccab3821558b84ff8dae5cb97c54b167f14c35c7601e25b9794667fd19eb255";
 	public $conn;
 
 	// get the database connection
