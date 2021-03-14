eed8dccde7effec93d716bf02ffd5a9d7d26d226
Lizel Graaff <Lizel.Graaff@universal.co.za>
2021-03-14 18:10:24 +0200

    v1

--- objects/product.php
+++ objects/product.php
@@ -28,11 +28,6 @@ class Product {
 		$this->price=htmlspecialchars(strip_tags($this->price));
 		$this->description=htmlspecialchars(strip_tags($this->description));
 
-		// bind values
-		// $stmt->bindParam(":name", $this->name);
-		// $stmt->bindParam(":price", $this->price);
-		// $stmt->bindParam(":description", $this->description);
-
 		// execute query
 		if ($stmt->execute()) {
 			return true;
@@ -49,7 +44,7 @@ class Product {
 	public function read() {
 
 		// select all query
-		$query = "SELECT  name, price, description	FROM products";
+		$query = "SELECT  name, price, description FROM products";
 
 		// prepare query statement
 		
@@ -82,11 +77,6 @@ class Product {
 		$this->price=htmlspecialchars(strip_tags($this->price));
 		$this->description=htmlspecialchars(strip_tags($this->description));
 
-		// bind new values
-		// $stmt->bindParam(':name', $this->name);
-		// $stmt->bindParam(':price', $this->price);
-		// $stmt->bindParam(':description', $this->description);
-
 		// execute the query
 		if ($stmt->execute()) {
 			return true;
@@ -99,7 +89,7 @@ class Product {
 	function delete() {
 
 		// delete query
-		$query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+		$query = "DELETE FROM " . $this->table_name . " WHERE id = $this->id";
 
 		// prepare query
 		$stmt = $this->conn->prepare($query);
@@ -108,7 +98,7 @@ class Product {
 		$this->id=htmlspecialchars(strip_tags($this->id));
 
 		// bind id of record to delete
-		$stmt->bindParam(1, $this->id);
+		//$stmt->bindParam(1, $this->id);
 
 		// execute query
 		if ($stmt->execute()) {
