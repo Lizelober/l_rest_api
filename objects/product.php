bf1e965addf62e9ccf8151367057e1135692d291
Lizel Graaff <Lizel.Graaff@universal.co.za>
2021-03-14 12:25:16 +0200

    v1

--- objects/product.php
+++ objects/product.php
@@ -10,9 +10,6 @@ class Product {
 	public $name;
 	public $description;
 	public $price;
-	public $category_id;
-	public $category_name;
-	public $created;
 
 	// constructor with $db as database connection
 	public function __construct($db) {
@@ -40,15 +37,11 @@ class Product {
 		$this->name=htmlspecialchars(strip_tags($this->name));
 		$this->price=htmlspecialchars(strip_tags($this->price));
 		$this->description=htmlspecialchars(strip_tags($this->description));
-		$this->category_id=htmlspecialchars(strip_tags($this->category_id));
-		$this->created=htmlspecialchars(strip_tags($this->created));
 
 		// bind values
 		$stmt->bindParam(":name", $this->name);
 		$stmt->bindParam(":price", $this->price);
 		$stmt->bindParam(":description", $this->description);
-		$stmt->bindParam(":category_id", $this->category_id);
-		$stmt->bindParam(":created", $this->created);
 
 		// execute query
 		if ($stmt->execute()) {
